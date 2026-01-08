562ce65c036325f58ec72725a346c9b5f070c906
Aman <ketan@Mac-mini.local>
2026-01-08 13:23:09 +0530

    Fix static asset delivery: add /static route and include CSS\n- Serve /static/* via Flask\n- Track Tailwind output.css and input.css\n- Include favicon.ico

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, send_file
+from flask import Blueprint, send_file, send_from_directory
 import os
 
 main_bp = Blueprint('main', __name__)
 
 # Assuming project root is parent directory of routes/
 FRONTEND_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+STATIC_DIR = os.path.join(FRONTEND_DIR, 'static')
 
 @main_bp.route('/', methods=['GET'])
 def root_page():
@@ -25,3 +26,7 @@ def admin_dashboard_js():
 @main_bp.route('/trainer.html', methods=['GET'])
 def trainer_page():
     return send_file(os.path.join(FRONTEND_DIR, 'trainer.html'))
+
+@main_bp.route('/static/<path:filename>', methods=['GET'])
+def static_files(filename):
+    return send_from_directory(STATIC_DIR, filename)
